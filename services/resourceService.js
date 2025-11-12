6afdfa1593f3b56212e8d2fdca559a54ec9e40bd
FilipiR22 <cruz.filipi@escolar.ifrn.edu.br>
2025-11-11 23:05:18 -0300

    alterando rota de api

--- services/resourceService.js
+++ services/resourceService.js
@@ -1,11 +1,12 @@
 import api from './api'
 
+const resources = 'resources'
+
 export default {
     list(params) {
-        // garante que os params vindos do componente sejam enviados ao json-server
-        return api.get('/resources', { params })
+        return api.get(`/${resources}`, { params })
     },
-    create(data) { return api.post('/resources', data) },
-    update(id, data) { return api.put(`/resources/${id}`, data) },
-    remove(id) { return api.delete(`/resources/${id}`) }
+    create(data) { return api.post(`/${resources}`, data) },
+    update(id, data) { return api.put(`/${resources}/${id}`, data) },
+    remove(id) { return api.delete(`/${resources}/${id}`) }
 }
