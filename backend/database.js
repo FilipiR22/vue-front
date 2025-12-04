770f2aadda0a1d68011d12b18e5b30d7bf784f7f
FilipiR22 <cruz.filipi@escolar.ifrn.edu.br>
2025-12-04 09:10:09 -0300

    removendo back end

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './meubanco.db',
-    logging: false,
-});
-
-export default sequelize;
